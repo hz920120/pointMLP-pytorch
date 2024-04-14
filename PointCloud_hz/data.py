a29629b05ee80cbc1e42e0fecfadf317dfc00715
Huang <hz920120@163.com>
2024-04-14 10:12:47 +0800

    commit file

--- PointCloud_hz/data.py
+++ PointCloud_hz/data.py
@@ -145,6 +145,9 @@ class TeethPointCloudData(Dataset):
         repeat_shape = list(idx.shape)
         repeat_shape[0] = 1
         batch_indices = torch.arange(B, dtype=torch.long).to(device).view(view_shape).repeat(repeat_shape)
+        points = points.to(device)
+        batch_indices = batch_indices.to(device)
+        idx = idx.to(device)
         points = points[batch_indices, idx, :]
         return points
 
